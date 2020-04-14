ed8fa7006d36e46151b56fafdee242d585035926
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-04-14 15:56:55 -0400

    Express Session Part 2

--- routes/users.js
+++ routes/users.js
@@ -11,26 +11,26 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', (req, res, next) => {
-  User.findOne({ username: req.body.username })
-    .then((user) => {
-      if (user) {
-        var error = new Error('User ' + req.body.username + ' already exisits');
-        err.status = 403;
-        next(err);
-      } else {
-        return User.create({
-          username: req.body.username,
-          password: req.body.password
-        });
-      }
-    })
-    .then(user => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({ status: 'Registration Successful', user: user })
-    }, err => next(err))
-    .catch(err => next(err))
-})
+  User.findOne({username: req.body.username})
+  .then((user) => {
+    if(user != null) {
+      var err = new Error('User ' + req.body.username + ' already exists!');
+      err.status = 403;
+      next(err);
+    }
+    else {
+      return User.create({
+        username: req.body.username,
+        password: req.body.password});
+    }
+  })
+  .then((user) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({status: 'Registration Successful!', user: user});
+  }, (err) => next(err))
+  .catch((err) => next(err));
+});
 
 router.post('/login', (req, res, next) => {
 
